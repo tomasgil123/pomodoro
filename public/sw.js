28f7e1ca0cf2501839819b8cd16498f3934262a3
Tomas Gil <tomasgil1234@gmail.com>
2020-11-21 08:15:55 -0300

    feat: add request to cache

--- public/sw.js
+++ public/sw.js
@@ -4,4 +4,14 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
   console.log(event.request)
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function (cache) {
+      return fetch(event.request).then(function (response) {
+        if (event.request.url.indexOf('http') === 0) {
+          cache.put(event.request, response.clone())
+        }
+        return response
+      })
+    })
+  )
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import PageWithLayout from 'src/types/pageWithLayout'
 
 const sendRequest = async (): Promise<void> => {
   const result = await (await fetch('https://jsonplaceholder.typicode.com/todos/2')).json()
+  const result2 = await (await fetch('http://localhost:3000/api/hello')).json()
   console.log('result', result)
 }
 
@@ -21,10 +22,37 @@ const Home: FC = () => {
     sendRequest()
   }
 
+  const onGetRequestFromCache = async (): Promise<void> => {
+    const test1 = await caches.keys()
+    const request = new Request('/api/hello')
+    const test2 = await (await caches.match(request)).json()
+    const test = await (await caches.match('https://jsonplaceholder.typicode.com/todos/2')).json()
+    debugger
+
+    caches
+      .match('/todos/2')
+      .then(function (response) {
+        if (!response) throw Error('No data')
+        return response.json()
+      })
+      .then(function (data) {
+        // don't overwrite newer network data
+        console.log('data', data)
+      })
+      .catch(function () {
+        // we didn't get cached data, the network is our last hope:
+        console.log('we didnt get cached data')
+      })
+      .catch((err) => {
+        console.log('err', err)
+      })
+  }
+
   return (
     <div>
       Home
       <button onClick={onSendRequest}>Make request</button>
+      <button onClick={onGetRequestFromCache}>Get request from cache</button>
       <Counter />
     </div>
   )
